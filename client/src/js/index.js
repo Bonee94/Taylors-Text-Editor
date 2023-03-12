4c2f68370a84d4a2a52f10b6b93004badd99ffea
Bonee94 <bonee94@yahoo.com>
2023-03-12 11:25:42 -0500

    Corrected name for workbox service worker

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,7 +3,6 @@ import Editor from './editor';
 import './database';
 import '../css/style.css';
 import logoImg from '../images/logo.png';
-console.log(logoImg);
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
@@ -28,7 +27,7 @@ if (typeof editor === 'undefined') {
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+  const workboxSW = new Workbox('/sw.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
