a39a42d2f49c7e0bbb51ad7c77bc1c1c7ff1d815
Bonee94 <bonee94@yahoo.com>
2023-03-12 16:28:25 -0500

    Created local storage check for already installed application

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,31 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
+let deferredPrompt;
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+const checkInstall = () => {
+  let installCheck = localStorage.getItem("app-installed");
+
+  console.log("this is the install check: ", installCheck);
+  //Checking local storage for boolean of app installed
+  if (installCheck === "true") {
+    console.log("app is installed, check ran");
+
+    butInstall.setAttribute("disabled", true);
+    butInstall.textContent = "Thank you for installing J.A.T.E.!";
+  } else {
+    startEvent();
+
+    // TODO: Add an handler for the `appinstalled` event
+    window.addEventListener("appinstalled", (event) => {
+      butInstall.textContent = "Thank you for installing J.A.T.E.!";
+      console.log("👍", "appinstalled", event);
+    });
+  }
+};
+
+checkInstall();
