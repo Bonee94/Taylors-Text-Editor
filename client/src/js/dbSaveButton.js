2999250a059694c84e11903523051e7be20da796
Bonee94 <bonee94@yahoo.com>
2023-03-12 19:30:03 -0500

    added alert display to save button wehn clicked

--- client/src/js/dbSaveButton.js
+++ client/src/js/dbSaveButton.js
@@ -1,7 +1,9 @@
 import { putDb } from './database'
+import { alertDisplay } from "./alertDisplay"
 
 document.getElementById('buttonSave')
 .addEventListener('click', () => {
+    alertDisplay("saved")
     console.log('saved content through button click')
     putDb(localStorage.getItem('content'))
 })
